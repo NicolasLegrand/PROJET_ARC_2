298def114d8df614c96ed43cbd6807a8529fb884
NicolasLegrand <nicolas1.legrand@gmail.com>
2014-02-04 15:10:03 +0000

    Modif avec nouvel accueil

--- index.php
+++ index.php
@@ -1,115 +1,96 @@
-		<?php include('includes/header.php'); ?>
-		
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/accueil_style.css">
+</head>
+<body>
+<?php error_reporting(0); ?>
+	<div id='contener_logo'>
+		<img src="images/logo_ARC.png">
+	</div>
 		<div id='contener'>
-		
-			<div id='introduction'>
-				<p>Chèrs (es) Clients (es),
-					Afin de mieux répondre à vos attentes,
-					ARC déménage son siège vers des locaux plus spacieux disposant d’un showroom présentant
-					les produits et solutions que nous vous proposons.
 
-					Afin de maintenir la proximité, 
-					l’ancienne adresse sise : 12, rue St-Michel, 
-					Tel : 221 33 849 8585 – Fax : 33 822 70 99, devient une succursale.
-				</p>
-			</div>
-				<!--<h1 class='prestation'>Nos prestations</h1>-->
-				<div class='inter_bloc'></div>
-			
-					<div id='integration'>
-						<div id='txt_integration'>
-							<h1>Intégrateur de solutions</h1>
-							<p><li>Infrastructure & Réseau LAN/WAN</li>
-								<li>Réseaux & Sécurité</li>
-								<li>Communication unifiée & ToIP</li>
-								<li>Applications d'entreprise</li>
-								<li>Sécurité physique</li>
-								<li>Editeur de logiciels</li>
-							</p>
-						</div>
-					</div>
-					<div id='img_integration'>
-							<img src='images/integration.jpg' />
-					</div>			
+			<ul id='bloc_accueil'>
+				<li>
+					<a href="principal.php">
+						<div class='icon'><span class='LigatureSymbols'>home</span></div>
+						<div class='content'>
+							<h2>Accueil</h2>
+							<div class='diagonale'>
+							</div>
+						</div>	
+					</a>
+				</li>
+			</ul>
 
-					<div class='inter_bloc'></div>
-					
-					<div id='formation'>
-						<div id='txt_formation'>
-							<h1>Formation Consulting</h1>
-								<p><li>Management de projet</li>
-									<li>Technologie Microsoft</li>
-									<li>Technologie Cisco</li>
-									<li>Technologie Linux</li>
-									<li>Solutions Open Source</li>
-									<li>Virtualisation & Reprise d'activité</li>
-								</p>
+			<ul id='bloc_prestations'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>memo</span></div>
+						<div class='content'>
+							<h2>Prestations</h2>
+							<div class='diagonale'>
+							</div>
 						</div>
-						<div id='img_formation'>
-							<img src='images/formation.png' />
+					</a>	
+				</li>
+			</ul>
+			
+			<ul id='bloc_produits'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>server</span></div>
+						<div class='content'>
+							<h2>Produits</h2>
+							<div class='diagonale'>
+							</div>
 						</div>
-					</div>
-					
-					<div class='inter_bloc'></div>
-					
-					<div id='fournisseur_internet'>
-						<div id='txt_fournisseur_internet'>
-							<h1>Fournisseur d'accès Internet</h1>
-								<p><li>Deuxième Fournisseur d'accès Internet<br/>&nbsp;&nbsp;&nbsp; au Sénégal depuis 1996<br/>
-										<br/>
-										&nbsp;&nbsp;&nbsp; Plus de 4000 abonnés :</li>
-										<ul>
-											<li>Entreprises</li>
-											<li>Professions libérales</li>
-											<li>Particuliers</li>
-										</ul>
-									<li>Offre de services à valeur ajoutée</li>
-								</p>
-						</div>
-						<div id='img_fournisseur_internet'>
-							<img src='images/fournisseur_internet.jpg' />
-						</div>
-					</div>			
+					</a>	
+				</li>
+			</ul>
 
-					<div class='inter_bloc'></div>				
-					
-					<div id='fournisseur_equipement'>
-						<h2>Fournisseur d'équipement</h2>
-							<p><li>Microsoft</li>
-								<li>Cisco</li>
-								<li>HP</li>
-								<li>Fujitsu SIEMENS</li>
-								<li>Acer</li>
-								<li>Sage</li>
-							</p>
-					</div>
-			
-			<div class='inter_bloc'></div>
-		
-			<div id='presentation'>
-				<p> La société ARC Informatique, 
-					créée en 1993, a réussi à hisser 
-					parmi les entreprises les plus 
-					performantes du Sénégal, 
-					il dispose d'une équipe 
-					d'ingénieurs expérimentés, 
-					maîtrisant l'Audit, le Design, 
-					la Gestion de Projets, 
-					et les Services managés.
-				</p>
-			</div>
-			
-			<div class='inter_bloc'></div>
-			
-			<div id='webmail'>
-				<p> Pour accèder à votre Webmail 
-					il vous suffit de taper l'adresse : 
-					<a href='http://webmail.arc.sn'>http://webmail.arc.sn</a> 
-					ou de cliquer sur l'icone ci-dessous.
-				</p>
-				<a href='http://webmail.arc.sn'><img class='logo_webmail' src='images/webmail.png'/></a>
-			</div>
-		
-			<div class='inter_bloc'></div>
-		
-		<?php include('includes/footer.php'); ?>	
+			<ul id='bloc_partenaires'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>group</span></div>
+						<div class='content'>
+							<h2>Partenaires</h2>
+							<div class='diagonale'>
+							</div>
+						</div>
+					</a>	
+				</li>
+			</ul>
+
+			<ul id='bloc_contact'>			
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'>mail</span></div>
+						<div class='content'>
+							<h2>Contact</h2>
+							<div class='diagonale'>
+							</div>
+						</div>
+					</a>	
+				</li>
+			</ul>
+
+			<ul id='bloc_webmail'>
+				<li>
+					<a href="#">
+						<div class='icon'><span class='LigatureSymbols'><strong>@</strong></span></div>
+						<div class='content'>
+							<h2>Webmail</h2>
+							<div class='diagonale'>
+							</div>
+						</div>
+					</a>	
+				</li>
+			</ul>
+		</div>
+		<div class='inter_bloc_accueil'></div>
+		<div id='footer_accueil'>
+			<?php include('includes/footer.php'); ?>
+		</div>	
+</body>
+</html>
